b8425be363adbeb4f554be7cfeb6ad88156c9688
Minh Mai <minhmanmai90@gmail.com>
2017-11-28 16:50:44 +1100

    Lecture 14 28-11-2017

--- app.js
+++ app.js
@@ -1,33 +1,24 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+
+var routes = require('./api/routes');
 
 app.set('port', 3000);
 
-app.get('/', function (req, res) {
-    console.log('GET the homepage');
-    res
-        .status(200)
-        .sendFile(path.join(__dirname, 'public', 'index.html'));
+app.use(function (req, res, next) {
+    console.log(req.method, req.url);
+    next();
 });
 
-app.get('/json', function (req, res) {
-    console.log('GET the json');
-    res
-        .status(200)
-        .json({"data": "test data"});
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/file', function (req, res) {
-    console.log('GET the file');
-    res
-        .status(200)
-        .sendFile(path.join(__dirname, 'app.js'));
-});
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/api', routes);
 
 var server = app.listen(app.get('port'), function () {
     var port = server.address().port;
     console.log('Listening to port: ' + port);
 });
-
-console.log('Me first!');
