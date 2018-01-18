b6ce8d15be96fc8f8cbde7661ca417c4e6b26988
Minh Mai <minhmanmai90@gmail.com>
2018-01-18 19:29:54 +1100

    TODO: complete error trapping for reviews

--- api/controllers/reviews.controller.js
+++ api/controllers/reviews.controller.js
@@ -10,10 +10,41 @@ module.exports.reviewsGetAll = function (req, res) {
         .findById(hotelId)
         .select('reviews')
         .exec(function (err, hotel) {
-            console.log('Return hotel ' + hotel);
+            var response = {
+                status: 200,
+                message: hotel
+            };
+            
+            if (err) {
+                console.log('Error finding hotels!');
+                response.status(400);
+                response.message = {
+                    "message": "Error finding hotels"
+                }
+            } else if (!hotel) {
+                console.log('Hotel not found!');
+                response.status = 400;
+                response.message = {
+                    "message": "Hotel not found!"
+                }
+            } else {
+                console.log('Return hotel ' + hotelId);
+                var reviews = hotel.reviews;
+                if (reviews){
+                    console.log('Return reviews' + reviews);
+                    response.status = 200;
+                    response.message = reviews
+                } else {
+                    console.log('No review found!');
+                    response.status = 200;
+                    response.message = {
+                        "message": "No review found!"
+                    }
+                }
+            }
             res
-                .status(200)
-                .json(hotel.reviews);
+                .status( response.status)
+                .json(response.message);
         });
 
 };
