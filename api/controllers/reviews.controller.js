d196045c303d4fdd039e0d089b5e41e69ff1f99b
Minh Mai <minhmanmai90@gmail.com>
2018-01-21 20:56:10 +1100

    finished error traps for reviews

--- api/controllers/reviews.controller.js
+++ api/controllers/reviews.controller.js
@@ -14,7 +14,7 @@ module.exports.reviewsGetAll = function (req, res) {
                 status: 200,
                 message: hotel
             };
-            
+
             if (err) {
                 console.log('Error finding hotels!');
                 response.status(400);
@@ -30,7 +30,7 @@ module.exports.reviewsGetAll = function (req, res) {
             } else {
                 console.log('Return hotel ' + hotelId);
                 var reviews = hotel.reviews;
-                if (reviews){
+                if (reviews) {
                     console.log('Return reviews' + reviews);
                     response.status = 200;
                     response.message = reviews
@@ -43,10 +43,9 @@ module.exports.reviewsGetAll = function (req, res) {
                 }
             }
             res
-                .status( response.status)
+                .status(response.status)
                 .json(response.message);
         });
-
 };
 
 //GET one review for a hotel
@@ -59,10 +58,40 @@ module.exports.reviewsGetOne = function (req, res) {
         .findById(hotelId)
         .select('reviews')
         .exec(function (err, hotel) {
-            console.log('Return hotel ' + hotel);
-            var review = hotel.reviews.id(reviewId);
+            var response = {
+                status: 200,
+                message: hotel
+            };
+
+            if (err) {
+                console.log('Error finding hotels!');
+                response.status(400);
+                response.message = {
+                    "message": "Error finding hotels"
+                }
+            } else if (!hotel) {
+                console.log('Hotel not found!');
+                response.status = 400;
+                response.message = {
+                    "message": "Hotel not found!"
+                }
+            } else {
+                console.log('Return hotel ' + hotelId);
+                var review = hotel.reviews.id(reviewId);
+                if (review) {
+                    console.log('Return review ' + review);
+                    response.status = 200;
+                    response.message = review
+                } else {
+                    console.log('Review not found!');
+                    response.status = 200;
+                    response.message = {
+                        "message": "Review not found!"
+                    }
+                }
+            }
             res
-                .status(200)
-                .json(review);
+                .status(response.status)
+                .json(response.message);
         });
 };
