d54eb3fcb82f54f88a3cfbfd36af7062b6464b52
Minh Mai <minhmanmai90@gmail.com>
2018-02-28 11:29:27 +1100

    starting lecture 44

--- fst-angular-mean/main/main-controller.js
+++ fst-angular-mean/main/main-controller.js
@@ -4,6 +4,8 @@ angular.module('myApp')
 function MainController(FilmFactory) {
     var vm = this;    
     vm.name = 'Tamas';
+    vm.date1 = "12 February 2016";
+    vm.date2 = "01 August 2018";
 
     FilmFactory.getAllFilms().then(function(response) {
         console.log(response);
