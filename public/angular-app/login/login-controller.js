4aab345f049afdbb79328e2f94645a66885cad27
Minh Mai <minhmanmai90@gmail.com>
2018-03-16 15:47:05 +1100

    continue lecture 52

--- public/angular-app/login/login-controller.js
+++ public/angular-app/login/login-controller.js
@@ -1,5 +1,38 @@
 angular.module("meanhotel").controller("LoginController", LoginController);
 
-function LoginController(){
+function LoginController($location, $http, $window, AuthFactory){
     var vm = this;
+    vm.isLoggedIn = function(){
+        return (AuthFactory.isLoggedIn ? true :false);
+    };
+
+    vm.login = function(){
+        if (vm.username && vm.password){
+            var user = {
+                username: vm.username,
+                password: vm.password
+            }
+        }
+        $http.post("/api/users/login/", user).then(function(response){
+            if (response.data.success){
+                $window.sessionStorage.token = response.data.token;
+                AuthFactory.isLoggedIn = true;
+                (vm.loggedinUser ? ", " + response.data.name : "")
+            }
+        }).catch(function(error){
+            console.log("Error logging in!", error);
+        })
+    };
+
+    vm.logout = function(){
+        AuthFactory.isLoggedIn = false;
+        delete $window.sessionStorage.token;
+        $location.path("/");
+        vm.loggedinUser = "";
+    }
+
+    vm.isActiveTab = function(url){
+        var currentPath = $location.path(). split("/")[1];
+        return (url === currentPath ? "active" : "");       
+    }
 }
